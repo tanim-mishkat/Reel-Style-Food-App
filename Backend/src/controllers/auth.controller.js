5170817a325c1547275a4f2b5516479c5f0ec6d4
tanim-mishkat <t5mishkat@gmail.com>
2025-09-04 14:18:51 +0600

    add food partner auth api

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,11 @@
 const userModel = require('../models/user.model.js')
+const foodPartnerModel = require('../models/foodpartner.model.js')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+
+
+
+// user auth controller
 
 async function registerUser(req, res) {
     const { fullName, email, password } = req.body
@@ -68,4 +73,78 @@ async function logoutUser(req, res) {
 }
 
 
-module.exports = { registerUser, loginUser, logoutUser }
+// food partner auth controller
+
+async function registerFoodPartner(req, res) {
+    const { fullName, email, password } = req.body
+    if (!fullName || !email) {
+        return res.status(400).json({ message: 'All fields are required' })
+    }
+    const isFoodPartnerExist = await foodPartnerModel.findOne({ email })
+    if (isFoodPartnerExist) {
+        return res.status(400).json({ message: 'Food partner already exist' })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const foodPartner = await foodPartnerModel.create({ fullName, email, password: hashedPassword })
+
+    const token = jwt.sign({ id: foodPartner._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+
+    res.cookie('token', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
+
+    res.status(201).json({
+        message: 'Food partner registered successfully',
+        foodPartner: {
+            _id: foodPartner._id,
+            fullName: foodPartner.fullName,
+            email: foodPartner.email
+        }
+    })
+}
+
+async function loginFoodPartner(req, res) {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'All fields are required' })
+    }
+
+    const foodPartner = await foodPartnerModel.findOne({ email })
+    if (!foodPartner) {
+        return res.status(400).json({ message: 'Invalid email or password' })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, foodPartner.password)
+    if (!isPasswordValid) {
+        return res.status(400).json({ message: 'Invalid email or password' })
+    }
+
+    const token = jwt.sign({ id: foodPartner._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+
+    res.cookie('token', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
+    res.status(200).json({
+        message: 'Food partner logged in successfully',
+        foodPartner: {
+            _id: foodPartner._id,
+            fullName: foodPartner.fullName,
+            email: foodPartner.email
+        }
+    })
+}
+
+
+async function logoutFoodPartner(req, res) {
+    res.clearCookie('token')
+    res.status(200).json({ message: 'Food partner logged out successfully' })
+}
+
+module.exports = {
+    registerUser,
+    loginUser,
+    logoutUser,
+    registerFoodPartner,
+    loginFoodPartner,
+    logoutFoodPartner
+}
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const authController = require('../controllers/auth.controller.js')
-const { register } = require('module')
 
 const router = express.Router()
+
+
+//user auth API
 
 router.post('/user/register', authController.registerUser)
 router.post('/user/login', authController.loginUser)
 router.get('/user/logout', authController.logoutUser)
+
+
+// food partner auth API
+
+router.post('/foodpartner/register', authController.registerFoodPartner)
+router.post('/foodpartner/login', authController.loginFoodPartner)
+router.get('/foodpartner/logout', authController.logoutFoodPartner)
 
 module.exports = router
