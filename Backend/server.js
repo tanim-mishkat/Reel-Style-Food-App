84fd31773fd459db70be1baf43255bc2c4a6f096
tanim-mishkat <t5mishkat@gmail.com>
2025-09-26 22:19:36 +0600

    .

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,12 @@ const http = require('http');
 const app = require('./src/app.js');
 const connectDB = require('./src/db/db.js');
 const { initRealtime } = require('./src/socket/index.js');
+const dotenv = require('dotenv').config()
+
 
 const PORT = process.env.PORT || 3000;
 
-connectDB();
+connectDB()
 
 const server = http.createServer(app);
 initRealtime(server);
